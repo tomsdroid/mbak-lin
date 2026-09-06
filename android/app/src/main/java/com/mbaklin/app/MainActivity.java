9f35b5b331ba527e5f2e73b127330225b3400f2d
Toms Droid <tomsdroid007@gmail.com>
2026-09-06 07:04:17 -0700

    ci: add Android release pipeline

---
+++ android/app/src/main/java/com/mbaklin/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.mbaklin.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mbaklin.app',
+  appName: 'MbakLin',
+  webDir: 'dist'
+};
+
+export default config;
